78ba5167167c1d14376ff755f2bfe39ea4d00145
kekraft <kekraft@users.noreply.github.com>
2016-04-16 13:31:48 -0700

    Update add_contamination_tool.cpp

--- contamination_monitor/src/add_contamination_tool.cpp
+++ contamination_monitor/src/add_contamination_tool.cpp
@@ -1,30 +1,13 @@
 /*
- * Copyright (c) 2011, Willow Garage, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+An RVIZ GUI tool for adding contamination to the contamination occupancy grid.
+
+The user selects the tool and clicks on the scene. A square mesh is added to the RVIZ scene.
+
+The points are then sent over the topic: /contamination_addition
+Using the current rviz tf id as the header for the points.
+
+Based largely on the tutorial for RVIZ GUI Tools found here:
+http://docs.ros.org/indigo/api/rviz_plugin_tutorials/html/tool_plugin_tutorial.html
  */
 
 #include <OGRE/OgreSceneNode.h>
@@ -199,30 +182,6 @@ int AddContaminationTool::processMouseEvent( rviz::ViewportMouseEvent& event )
   bool success = context_->getSelectionManager()->get3DPoint( event.viewport, event.x, event.y, pos );
   // setCursor( success ? hit_cursor_ : std_cursor_ );
 
-  /*
-  if ( event.leftDown() )
-  {
-    //std::cout << "Button clicked" << std::endl;
-    std::ostringstream s;
-    s << "<b>Left-Click:</b> Select this point.";
-    s.precision(3);
-    s << " [" << pos.x << "," << pos.y << "," << pos.z << "]";
-    setStatus( s.str().c_str() );
-
-    geometry_msgs::PointStamped ps;
-    ps.point.x = pos.x;
-    ps.point.y = pos.y;
-    ps.point.z = pos.z;
-    ps.header.frame_id = context_->getFixedFrame().toStdString();
-    ps.header.stamp = ros::Time::now();
-    point_pub.publish( ps );
-
-  }
-  else
-  {
-    setStatus( "Click 2 points to create a rectangle." );
-  } 
-  */
 
   std::ostringstream s;
   s << "<b>Left-Click:</b> Select this point.";
@@ -250,13 +209,6 @@ int AddContaminationTool::processMouseEvent( rviz::ViewportMouseEvent& event )
     {
       // makeContamRect( intersection );
       current_rect_property_ = NULL; // Drop the reference so that deactivate() won't remove it.
-
-      //std::cout << "Button clicked" << std::endl;
-      // std::ostringstream s;
-      // s << "<b>Left-Click:</b> Select this point.";
-      // s.precision(3);
-      // s << " [" << pos.x << "," << pos.y << "," << pos.z << "]";
-      // setStatus( s.str().c_str() );
 
       std::cout << s.str().c_str() << std::endl;
 
@@ -446,4 +398,3 @@ std::vector<Ogre::Vector3> AddContaminationTool::getRectDimensions(Ogre::Vector3
 
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_EXPORT_CLASS(contamination_monitor::AddContaminationTool,rviz::Tool )
-// END_TUTORIAL
